52ddc1d54f6c43340fb2cd2fcb2af020c8b1780a
nadasoliman2 <nadaemad271@gmail.com>
2026-03-17 23:02:24 +0200

    updates

--- src/common/utils/email/send.email.js
+++ src/common/utils/email/send.email.js
@@ -1,31 +1,32 @@
 import nodemailer from "nodemailer";
-import {EMAIL_PORT,EMAIL_HOST, EMAIL_USER , EMAIL_PASS,APPLICATION_NAME} from "../../../../config/config.service.js"
+import { EMAIL_PORT, EMAIL_HOST, EMAIL_USER, EMAIL_PASS, APPLICATION_NAME } from "../../../../config/config.service.js";
 
-export const sendEmail=async({
-    to,
-cc,
-bcc,
-subject,
-attachments=[],
-html}={})=>{ 
+export const sendEmail = async ({ to, cc, bcc, subject, attachments = [], html } = {}) => {
+  try {
     const transporter = nodemailer.createTransport({
-secure:false ,
-host:EMAIL_HOST,
-port:parseInt(EMAIL_PORT),
-  auth: {
-    user: EMAIL_USER,
-    pass: EMAIL_PASS,
-  },
-});
-  const info = await transporter.sendMail({
-    from: `"${APPLICATION_NAME}" <${EMAIL_USER}>`,
-    to,
-    subject,
-    cc, 
-    bcc,
-    html, 
-    attachments
-  });
+      host: EMAIL_HOST,
+      port: parseInt(EMAIL_PORT),
+      secure: false, // false for 587
+      auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASS, // لازم يكون App Password لو Gmail
+      },
+    });
 
-   console.log("Email sent:", info.messageId);
-}
+    const info = await transporter.sendMail({
+      from: `"${APPLICATION_NAME}" <${EMAIL_USER}>`,
+      to,
+      subject,
+      cc,
+      bcc,
+      html,
+      attachments,
+    });
+
+    console.log("Email sent:", info.messageId);
+    return info;
+  } catch (err) {
+    console.error("Error sending email:", err);
+    throw new Error("Failed to send email"); // ده يمنع 500 غير مفسر
+  }
+};
