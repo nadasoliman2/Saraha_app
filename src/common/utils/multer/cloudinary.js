43cbf4cee1fa33d5983204098a764df0618c93f8
nadasoliman2 <nadaemad271@gmail.com>
2026-04-13 03:33:10 +0200

    updates

--- src/common/utils/multer/cloudinary.js
+++ src/common/utils/multer/cloudinary.js
@@ -21,17 +21,18 @@ export const uploadFile =async({file ={},path="general"}={})=>{
 export const destroy = async ({public_id = ""}={})=>{
     return await cloud().uploader.destroy(public_id)
 }
-export const uploadFiles = async ({ files = [], path = "general" } = {}) => {
-  const attachments = [];
+export const uploadFile = async ({ file, path = "general" } = {}) => {
+  return await new Promise((resolve, reject) => {
+    const stream = cloud().uploader.upload_stream(
+      {
+        folder: `${process.env.APPLICATION_NAME}/${path}`
+      },
+      (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      }
+    );
 
-  for (const file of files) {
-    const { secure_url, public_id } = await uploadFile({
-      file: file.buffer,   // 👈 أهم تعديل
-      path
-    });
-
-    attachments.push({ secure_url, public_id });
-  }
-
-  return attachments;
+    stream.end(file); // 👈 buffer هنا
+  });
 };
